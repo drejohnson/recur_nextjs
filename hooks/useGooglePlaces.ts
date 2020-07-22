39aa1c1bb12a0fc61bc8be146dadb7202c4d2cff
Dre Johnson <dre@phreshr.com>
2020-07-23 00:49:11 +0800

    clean up

--- hooks/useGooglePlaces.ts
+++ hooks/useGooglePlaces.ts
@@ -8,25 +8,16 @@ export function useGooglePlaces(
   const google = useGoogleMapsApi(apiKey, { placesApi: true });
   const inputRef = useRef<HTMLInputElement>(null);
   const [autoComplete, setAutoComplete] = useState<any>();
-  // let autoComplete = null;
 
   useEffect(() => {
     if (!google || !inputRef) {
       return;
     }
-    // autoComplete = new google.maps.places.Autocomplete(
-    //   inputRef.current,
-    //   options
-    // );
     setAutoComplete(
       new google.maps.places.Autocomplete(inputRef.current, options)
     );
     autoComplete?.setFields(["address_component"]);
   }, [google, inputRef]);
-
-  // useEffect(() => {
-  //   console.log("autoComplete from hook", autoComplete);
-  // }, [autoComplete]);
 
   return { inputRef, autoComplete, google };
 }
